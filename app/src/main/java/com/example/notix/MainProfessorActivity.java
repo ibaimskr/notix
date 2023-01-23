4f4de7653d31d66f83d293460eb7ab335b73a4df
in2dm3-v <ibai.gonzalezug@elorrieta-errekamari.com>
2023-01-23 18:34:02 +0100

    ProfessorMain

---
+++ app/src/main/java/com/example/notix/MainProfessorActivity.java
@@ -0,0 +1,32 @@
+package com.example.notix;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainProfessorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_professor);
+
+        Bundle extras = getIntent().getExtras();
+        String access = extras.getString("access");
+        String dni = extras.getString("dni");
+
+        Button buttonNotes = findViewById(R.id.buttonProfessorNotes);
+        Button buttonAbsences = findViewById(R.id.buttonProfessorAbsences);
+        Button buttonReunion = findViewById(R.id.buttonProfessorReunion);
+
+        buttonNotes.setOnClickListener(view -> {
+            Intent i = new Intent(MainProfessorActivity.this, ProfessorNotesActivity.class);
+            i.putExtra("access", access);
+            i.putExtra("dni", dni);
+            startActivity(i);
+        });
+
+    }
+}
